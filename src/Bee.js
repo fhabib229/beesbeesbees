73ef739e6a2d08ff1aa358a8a3b601a45b8d614e
Faris Habib <faris.habib.7@gmail.com>
2018-08-31 18:53:56 -0700

    Passed all Bee class tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,8 +1,10 @@
 var Bee = function() {
-  var newBee = Grub();
+  Grub.call(this);
 
   this.age = 5;
   this.color = "yellow";
-  // this.food = Grub.food;
-
+  this.job = "keep on growing";
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
